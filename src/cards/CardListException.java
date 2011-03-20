9ce51a0a22b069f523209d6ce92f18377f1236ac
Justin <Fission@live.com>
2011-03-19 23:04:53 -0400

    Cards Subsystem

---
+++ src/cards/CardListException.java
@@ -0,0 +1,15 @@
+package cards;
+
+/**
+ * Indicates an attempt to draw a card from an empty deck.
+ */
+@SuppressWarnings("serial")
+public class CardListException extends RuntimeException  {
+	/**
+	 * Generates a standard error message.
+	 * @param pMessage The message to store.
+	 */
+	public CardListException(String pMessage) {
+		super(pMessage);
+	}
+}
