ca91b0b59b0fffb114b747a64d70be7ce3a1c984
Gabriel <gabriel.forgues@hotmail.com>
2011-03-24 21:37:22 -0400

    Revert "updated Blackjack"
    This reverts commit 769d9d5230154d325c979c19c6499be0100f5f7a.

---
+++ src/blackjackgame/Blackjack.java
@@ -0,0 +1,72 @@
+
+package blackjackgame;
+
+import cards.Card;
+import cards.CardList;
+import cards.Deck;
+
+/**
+ *
+ * @author Client
+ */
+public class Blackjack {
+
+    private Player player;
+    private Player dealer;
+    private Deck deckCards = new Deck();
+    private CardList playerHand;
+    private CardList dealerHand;
+
+    public void deal(){
+        for(int i=0; i>=2; i++){
+        Card drawnCardPlayer = deckCards.draw();
+        playerHand.add(drawnCardPlayer);
+        Card drawnCardDealer = deckCards.draw();
+        dealerHand.add(drawnCardDealer);
+        }
+
+    }
+    public Card hit(CardList hand){
+        Card hitCard = deckCards.draw();
+        hand.add(hitCard);
+        return hitCard;
+    }
+    public CardList stand(CardList hand){
+        return hand;
+    }
+
+    public void doubleDown(){
+    		
+    }
+
+    public void split(){
+
+    }
+
+    public void dealerDoes(){
+
+    }
+
+    public void bet(){
+
+    }
+    public int getPoints(CardList hand){
+        int points;
+        
+        return 55;
+    }
+
+    public void checkBust(CardList hand){
+        
+    }
+
+    public void checkBlackjack(CardList hand){
+
+    }
+
+    public void checkWin(CardList hand){
+
+    }
+
+
+}
