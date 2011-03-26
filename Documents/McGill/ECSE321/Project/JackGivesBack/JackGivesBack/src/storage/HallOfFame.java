fc3a719120e1755e5b6d1d7d1e9577fc3168e4b2
Gabriel <gabriel.forgues@hotmail.com>
2011-03-25 21:52:36 -0400

    Made Storage.java slightly easier to read, and added HallOfFame.

---
+++ Documents/McGill/ECSE321/Project/JackGivesBack/JackGivesBack/src/storage/HallOfFame.java
@@ -0,0 +1,62 @@
+package storage;
+
+import java.io.*;
+import java.util.*;
+
+public class HallOfFame {
+	
+	private static Storage findLowestPlayer(ArrayList<Storage> topFive) {
+		Storage lowestPlayer = topFive.get(0);
+		int currentMinimum = 0;
+		
+		for (Storage player : topFive) {
+			currentMinimum = player.getTotalChipsWon();
+			if (currentMinimum < lowestPlayer.getTotalChipsWon()) {
+				lowestPlayer = player;
+			}
+		}
+		return lowestPlayer;
+	}
+	private static ArrayList<Storage> compareWithTopFive(Storage playerToAdd, ArrayList<Storage> topFive) {
+		if (topFive.size() < 5) {
+			topFive.add(playerToAdd);
+		} else {
+			Storage lowestPlayer = findLowestPlayer(topFive);
+			if (playerToAdd.getTotalChipsWon() > lowestPlayer.getTotalChipsWon()) {
+				topFive.remove(lowestPlayer);
+				topFive.add(playerToAdd);
+			}
+		}
+		return topFive;
+	}
+	
+	public static void getHallOfFame() {
+		Scanner profileReader;
+		String username;
+		String password;
+		String parsedLine[];
+		File directory = new File("profiles");
+		File profiles[] = directory.listFiles();
+		ArrayList<Storage> topFive = new ArrayList<Storage>();
+		
+		for (File profile : profiles) {
+			Storage player;
+			username = profile.getName();
+			try {
+				profileReader = new Scanner(new File("profiles/" + username));
+				parsedLine = profileReader.nextLine().split(",");
+				password = parsedLine[0];
+				username = username.substring(0, username.lastIndexOf('.'));
+				player = Storage.loadPlayer(username, password);
+			} catch (FileNotFoundException e) {
+				continue;
+			}
+			topFive = compareWithTopFive(player, topFive);
+		}
+		
+		for (Storage topPlayer : topFive) {
+			System.out.print(topPlayer.getUsername() + ": ");
+			System.out.println(topPlayer.getTotalChipsWon());
+		}
+	}
+}
