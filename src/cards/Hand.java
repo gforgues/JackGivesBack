dc3e373d999e76ec739c4f1b868da263bd63a94c
Justin <Fission@live.com>
2011-03-31 09:05:19 -0400

    Added a boolean variable in Hand to keep track whether or not a Hand is done playing (ie when Stand has been played)

--- src/cards/Hand.java
+++ src/cards/Hand.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Hand implements Cloneable {
 	ArrayList<Card> hand;
+	boolean playable = true;
 	
 	public Hand() {
 		hand = new ArrayList<Card>();
@@ -31,6 +32,21 @@ public class Hand implements Cloneable {
 	
 	public int getNumberCards() {
 		return hand.size();
+	}
+	
+	/**
+	 * Sets the Hand's playable to be false once Stand has been played
+	 */
+	public void setDone() {
+		playable = false;
+	}
+	
+	/**
+	 * Checks whether Stand has been played on Hand 
+	 * @return playable True if Stand has not been played, false if it has
+	 */
+	public boolean isPlayable() {
+		return playable;
 	}
 	
 	/**
